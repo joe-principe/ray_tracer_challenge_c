bafdc8d4392ac086bc3033f9371d2974f2f9fd94
joe-principe <j.principe343@gmail.com>
2024-03-15 19:16:51 -0700

    Working on intersection struct and intersection list

--- 05_Ray-Sphere_Intersections/main.c
+++ 05_Ray-Sphere_Intersections/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <cglm/cglm.h>
 
 typedef struct ray ray;
 typedef struct sphere sphere;
+typedef struct intersection intersection;
+typedef struct intersection_list intersection_list;
+typedef void (*intersect_fp)(ray *, void *, intersection_list *);
 
 struct ray
 {
@@ -17,8 +21,28 @@ struct sphere
     vec3 origin;
 };
 
+struct intersection
+{
+    float t;
+    void *object;
+    intersect_fp intersect_func;
+};
+
+struct intersection_list
+{
+    int count;
+    int length;
+    intersection **intersections;
+};
+
 void ray_position(ray *r, float t, float *dest);
-void intersect(ray *r, sphere *s, float *dest);
+void sphere_intersect(ray *r, void *obj, intersection_list *il);
+intersection_list *create_intersection_list(void);
+void delete_intersection_list(intersection_list *il);
+void intersection_list_add(intersection_list *il, intersection *it);
+void intersection_list_remove(intersection_list *il);
+intersection *create_intersection(void);
+void delete_intersection(intersection *it);
 
 int
 main(void)
@@ -29,22 +53,24 @@ main(void)
     ray r4 = {GLM_VEC3_ZERO_INIT, {0.0f, 0.0f, 1.0f}};
     ray r5 = {{0.0f, 0.0f, 5.0f}, {0.0f, 0.0f, 1.0f}};
     sphere s = {1.0f, GLM_VEC3_ZERO_INIT};
-    vec2 intersections = GLM_VEC2_ZERO_INIT;
+    intersection_list *il = create_intersection_list();
 
-    intersect(&r1, &s, intersections);
-    printf("%.2f, %.2f\n", intersections[0], intersections[1]);
+    sphere_intersect(&r1, &s, il);
+    printf("%.2f, %.2f\n", il->intersections[0]->t, il->intersections[1]->t);
 
-    intersect(&r2, &s, intersections);
-    printf("%.2f, %.2f\n", intersections[0], intersections[1]);
+    sphere_intersect(&r2, &s, il);
+    printf("%.2f, %.2f\n", il->intersections[0]->t, il->intersections[1]->t);
 
-    intersect(&r3, &s, intersections);
-    printf("%.2f, %.2f\n", intersections[0], intersections[1]);
+    sphere_intersect(&r3, &s, il);
+    printf("%.2f, %.2f\n", il->intersections[0]->t, il->intersections[1]->t);
 
-    intersect(&r4, &s, intersections);
-    printf("%.2f, %.2f\n", intersections[0], intersections[1]);
+    sphere_intersect(&r4, &s, il);
+    printf("%.2f, %.2f\n", il->intersections[0]->t, il->intersections[1]->t);
 
-    intersect(&r5, &s, intersections);
-    printf("%.2f, %.2f\n", intersections[0], intersections[1]);
+    sphere_intersect(&r5, &s, il);
+    printf("%.2f, %.2f\n", il->intersections[0]->t, il->intersections[1]->t);
+
+    delete_intersection_list(il);
 
     return 0;
 }
@@ -57,11 +83,12 @@ ray_position(ray *r, float t, float *dest)
 }
 
 void
-intersect(ray *r, sphere *s, float *dest)
+sphere_intersect(ray *r, void *obj, intersection_list *il)
 {
     float a, b, c;
     float discriminant;
     vec3 sphere_to_ray;
+    sphere *s = (sphere *)obj;
 
     glm_vec3_sub(r->origin, s->origin, sphere_to_ray);
 
@@ -71,12 +98,124 @@ intersect(ray *r, sphere *s, float *dest)
 
     discriminant = b * b - 4 * a * c;
 
-    if (discriminant < 0) {
-        glm_vec2_zero(dest);
-        return;
+    if (discriminant == 0) {
+        intersection_list_add(il, create_intersection());
+        il->intersections[il->count]->t = (-b - sqrtf(discriminant))
+                                          / (2.0f * a);
+    } else if (discriminant > 0) {
+        intersection_list_add(il, create_intersection());
+        intersection_list_add(il, create_intersection());
+        il->intersections[il->count]->t = (-b - sqrtf(discriminant))
+                                          / (2.0f * a);
+        il->intersections[il->count]->t = (-b + sqrtf(discriminant))
+                                          / (2.0f * a);
+    }
+}
+
+intersection_list *
+create_intersection_list(void)
+{
+    intersection_list *il = malloc(sizeof(*il));
+
+    if (il == NULL) {
+        fprintf(stderr, "Error: Could not allocate enough memory for "
+                "intersection list\n");
+        exit(EXIT_FAILURE);
     }
 
-    dest[0] = (-b - sqrtf(discriminant)) / (2.0f * a);
-    dest[1] = (-b + sqrtf(discriminant)) / (2.0f * a);
+    il->length = 8;
+    il->count = 0;
+    il->intersections = calloc(il->length, sizeof(il->intersections));
+
+    if (il->intersections == NULL) {
+        fprintf(stderr, "Error: Could not allocate enough memory for "
+                "intersection list array\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return il;
+}
+
+void
+delete_intersection_list(intersection_list *il)
+{
+    int i;
+
+    for (i = 0; i < il->count; i++) {
+        delete_intersection(il->intersections[i]);
+    }
+
+    free(il->intersections);
+    il->intersections = NULL;
+
+    free(il);
+    il = NULL;
+}
+
+void
+intersection_list_add(intersection_list *il, intersection *it)
+{
+    intersection *temp = NULL;
+
+    if (il->count == il->length) {
+        temp = realloc(il->intersections,
+                       sizeof(il->intersections) * il->length * 2);
+
+        if (temp == NULL) {
+            fprintf(stderr, "Error: Could not reallocate memory for "
+                    "intersection list array increase\n");
+            exit(EXIT_FAILURE);
+        }
+
+        il->intersections = &temp;
+        il->length *= 2;
+    }
+
+    il->intersections[il->count] = it;
+    il->count++;
+}
+
+void
+intersection_list_remove(intersection_list *il)
+{
+    intersection *temp = NULL;
+
+    il->intersections[il->count] = NULL;
+    il->count--;
+
+    if (il->count == il->length / 4 && il->length > 8) {
+        temp = realloc(il->intersections,
+                       sizeof(il->intersections) * il->length * 2);
+
+        if (temp == NULL) {
+            fprintf(stderr, "Error: Could not reallocate memory for "
+                    "intersection list array decrease\n");
+            exit(EXIT_FAILURE);
+        }
+
+        il->intersections = &temp;
+        il->length /= 2;
+    }
+}
+
+intersection *
+create_intersection(void)
+{
+    intersection *it = malloc(sizeof(*it));
+
+    if (it == NULL) {
+        fprintf(stderr, "Error: Could not allocate enough memory for "
+                "intersection\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return it;
+}
+
+void
+delete_intersection(intersection *it)
+{
+    free(it);
+    it = NULL;
 }
 /* EOF */
